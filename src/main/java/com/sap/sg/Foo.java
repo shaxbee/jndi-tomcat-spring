b1fe3328daa098668fe2b1bd109bb45b3046420f
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2014-02-24 17:56:52 +0800

    Initial commit

---
+++ src/main/java/com/sap/sg/Foo.java
@@ -0,0 +1,26 @@
+package com.sap.sg;
+
+public class Foo {
+	
+	private String source_;
+    private String target_;
+	
+	public Foo() {
+	}
+	
+	public String getSource() {
+		return source_;
+	}
+
+    public void setSource(final String source) {
+        source_ = source;
+    }
+
+    public String getTarget() {
+        return target_;
+    }
+
+    public void setTarget(final String target) {
+        target_ = target;
+    }
+}
